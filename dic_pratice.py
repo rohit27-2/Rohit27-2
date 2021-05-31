687b2260e45a66c400ba6cc79ec31436bc3aebb7
Rohit27-2 <70756703+Rohit27-2@users.noreply.github.com>
2021-05-31 13:26:32 +0530

    Add files via upload

---
+++ dic_pratice.py
@@ -0,0 +1,23 @@
+import json
+
+data = json.load(open("Python_Projects/data.json"))
+
+def translate(word):
+    if word in data:
+        return data[word]
+    #Title
+    elif word.title() in data:
+        return data[word.title()]
+    #captital letters
+    elif data.upper() in data:
+        return data[word.upper()]
+    
+word = input("Enter the word you wannna search : ")
+output = translate(word)
+if type(output) == list :
+    for item in output:
+        print(item)
+else:
+    print(output)
+
+    
